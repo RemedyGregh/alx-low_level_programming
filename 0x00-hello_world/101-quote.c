87b491c5a2d3969f0821803aba0874f2fea359b4
RemedyGregh <ulokwemgregoryejike@gmail.com>
2022-09-08 21:46:07 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
  * main - Entry point
  *
  * Description: A program to print without put and printf function
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 
 }
