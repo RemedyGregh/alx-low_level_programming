0ba2bce20b4a669b4f1cc49429be13f3b866e276
RemedyGregh <ulokwemgregoryejike@gmail.com>
2022-09-22 02:50:33 -0700

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include "main'h"
+#include "main.h"
 
 /**
  * _strncpy - C function that copies a string including
@@ -18,7 +18,7 @@ char *_strncpy(char *dest, char *src, int n)
 		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[i] = src[i];
+		dest[index] = src[index];
 
 	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
