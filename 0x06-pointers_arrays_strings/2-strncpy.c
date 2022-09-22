ea3b0d1d214dec54d9ff840db1439cdd68f71329
RemedyGregh <ulokwemgregoryejike@gmail.com>
2022-09-21 19:41:08 -0700

    Task 2 abd 4

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,18 +7,21 @@
  * @src: the source string
  * @n: max number of byte copied
  *
- * Return: returns
+ * Return: a pointer to the resulting string dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+	int index = 0, src_len = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (src[index++])
+		src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
 		dest[i] = src[i];
 
-	for ( ; i < n; i++)
-		dest[i] = '\0';
+	for (index = src_len; index < n; index++)
+		dest[index] = '\0';
 
 	return (dest);
 }
