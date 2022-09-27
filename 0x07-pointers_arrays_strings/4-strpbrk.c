e278da1ed6eb41f7a1a37444e336987fa9119f8b
RemedyGregh <ulokwemgregoryejike@gmail.com>
2022-09-26 20:00:19 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any set of bytes
+ * @s: first occurrence of string to be located
+ * @accept: string to be located
+ *
+ * Return: Returns matching character in s or null 
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+
+	while (*s)
+	{
+		for (i= 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+	return ('\0');
