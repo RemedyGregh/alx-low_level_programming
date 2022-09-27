d40318ee6c96c496cce8da03fd16565baaa30be5
RemedyGregh <ulokwemgregoryejike@gmail.com>
2022-09-26 18:00:39 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memset fills the memory with constand bytes
+ * _memset - fills the memory with constand bytes
  * @s: address to memory block
  * @b: char to be used
  * @n: number of bytes to be used
