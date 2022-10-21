7fe91d9532cc72baeffdd48891c61a23a1de7adf
RemedyGregh <ulokwemgregoryejike@gmail.com>
2022-10-20 22:37:13 -0700

    corrected task 1 and 2

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,27 +1,20 @@
 #include "lists.h"
 
 /**
-* add_node - Function to add new node at the begining of the list
-* @head: Pointer pointing to first node in list
-* @str: Data elemnt to initialize new node
+* list_len - To get no of elements in a list
+* @h: Pointer to list or first node in list
 *
-* Return: Pointer to new node/element
+* Return: Number of elements in list
 */
-list_t *add_node(list_t **head, const char *str)
+size_t list_len(const list_t *h)
 {
-	size_t len = 0;
-	list_t *new_node;
+	size_t i;
 
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-		return (NULL);
-
-	while (str[len])
-		len++;
-	new_node->str = strdup(str);
-	new_node->len = len;
-	new_node->next = (*head);
-
-	*head = new_node;
-	return (*head);
+	i = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,39 +1,27 @@
 #include "lists.h"
 
 /**
-* add_node_end - Adds node to end of list
-* @head: Pointer pointing to list/ first node in list
-* @str: Pointer to new data element to insert
+* add_node - Function to add new node at the begining of the list
+* @head: Pointer pointing to first node in list
+* @str: Data elemnt to initialize new node
 *
-* Return: Address of new element or null
+* Return: Pointer to new node/element
 */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
 	size_t len = 0;
-	list_t *last = *head;
-
-	while (str[len])
-		len++;
+	list_t *new_node;
 
 	new_node = malloc(sizeof(list_t));
 	if (!new_node)
 		return (NULL);
 
+	while (str[len])
+		len++;
 	new_node->str = strdup(str);
 	new_node->len = len;
-	new_node->next = NULL;
+	new_node->next = (*head);
 
-	if (*head == NULL)
-	{
-		*head = new_node;
-		return (new_node);
-	}
-
-	while (last->next)
-		last = last->next;
-
-	last->next = new_node;
-
-	return (new_node);
-}}
+	*head = new_node;
+	return (*head);
+}
